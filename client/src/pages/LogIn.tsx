cffba02df12bb0fd9697b5a2d7c18c6d3e1bb1da
Hoa <sullivan.thuyhoa@gmail.com>
2025-04-03 14:28:34 -0500

    create users table and fixed the login form UI

--- client/src/pages/LogIn.tsx
+++ client/src/pages/LogIn.tsx
@@ -1,5 +1,20 @@
-function LogIn () {
-    return <div></div>
-}
+
+
+// function LogIn () {
+//     return <div></div>
+// }
+
+// export default LogIn;
+
+import LoginForm from "../components/loginform";
+
+const LogIn = () => {
+    return (
+        <div>
+            <h1>Log In</h1>
+            <LoginForm />
+        </div>
+    );
+};
 
 export default LogIn;
