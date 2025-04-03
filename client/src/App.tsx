20654e2747e7654d6b363e192cc33f387da8c3a0
Hoa <sullivan.thuyhoa@gmail.com>
2025-04-03 17:44:02 -0500

    adding route for LogIn page in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //Hoa 
+import LogIn from "./pages/LogIn";
+
 import { Outlet } from "react-router-dom";
 import Header from "./components/header";
 import Footer from "./components/footer";
-import LoginForm from "./components/loginform";
 
 function App() {
   return (
-    <>
-      <Header />
-        <main>
-          <Outlet />
-          <LoginForm />
-        </main>
-      <Footer />
-    </>
+
+    <Router>
+            <Routes>
+                <Route path="/login" element={<LogIn />} />
+                
+            </Routes>
+    </Router>
+    // <>
+    //   <Header />
+    //     <main>
+    //       <Outlet />
+    //     </main>
+    //   <Footer />
+    // </>
   )
 }
 
