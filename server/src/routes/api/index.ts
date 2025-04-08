4b60efce8592b2e408ebeb7b9b2e2b6237613b74
Hoa <sullivan.thuyhoa@gmail.com>
2025-04-07 21:45:36 -0500

    updated login

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import { commentRouter } from './comment-routes.js';
+import { commentRouter } from './comment-routes.js';  // No need to add .ts, TypeScript handles it automatically
 
 const router = express.Router();
 
-router.use('/comment', commentRouter);
+router.use('/api/comment', commentRouter);
 
 export default router;
--- server/src/routes/api/user-routes.ts
+++ server/src/routes/api/user-routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { User } from '../../models/userdata';
+import { User } from '../../models/userdata.js';
 
 const router = express.Router();
 
