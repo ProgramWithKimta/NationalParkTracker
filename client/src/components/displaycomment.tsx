69e687942ca8f36e5859cde059aeb7ede4e98057
ProgramWithKimta <knguyen4268@gmail.com>
2025-04-08 20:26:57 -0500

    updating comment styling

--- client/src/components/displaycomment.tsx
+++ client/src/components/displaycomment.tsx
@@ -2,24 +2,25 @@
 import { useEffect, useState } from "react";
 import { retrieveComments } from "../api/commentsAPI";
 
-const DisplayComment= () => {
-    const [comments, setComments] = useState([])
+const DisplayComment = () => {
+        const [comments, setComments] = useState<{ comment: string }[]>([]);
     useEffect(() => {
         async function getComments() {
-            const data = await retrieveComments()
+            const data = await retrieveComments();
             setComments(data);
         }
         getComments();
-    }, []) 
-    return <div className="display-comm-box">
-        {comments.map((c: {comment: string}) => {
-            return (
-            <div>
-            <p className="display-comment"> {c.comment} </p>
-            </div>    
-            )
-        })}
-    </div>
-}
-
-export default DisplayComment;
+    }, [])
+    return (
+      <div className="display-comm-box">
+        <h4>Park Reviews</h4>
+        <ul id="comment-list" className="display-comment">
+          {comments.map((c, index) => (
+            <li key={index}>{c.comment}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+  export default DisplayComment;
