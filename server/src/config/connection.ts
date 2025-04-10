bf9467eeb6ef82ba1ebbe9a8b744d55274cf1e17
ProgramWithKimta <74941366+ProgramWithKimta@users.noreply.github.com>
2025-04-09 21:24:02 -0500

    Update connection.ts

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,10 +1,8 @@
 import dotenv from 'dotenv';
-import { Sequelize } from 'sequelize';
 
-
-// Load the .env file from the server directory
 dotenv.config();
 
+import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(process.env.DATABASE_URL!, {
   port: Number(process.env.DB_PORT ?? 5432), // Default to 5432 for PostgreSQL
